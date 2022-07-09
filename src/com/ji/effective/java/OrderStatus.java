88712d9cc8bcd47b8170e58220a852cdc449d58b
SeonHakJi <jshag90@naver.com>
2022-07-09 18:22:29 +0900

    [Add] enum 개념 설명 및 Quiz 정리

--- src/com/ji/effective/java/OrderStatus.java
+++ src/com/ji/effective/java/OrderStatus.java
@@ -2,5 +2,12 @@ package com.ji.effective.java;
 
 public enum OrderStatus {
 
+    PREPARING(0), SHIPPED(1), DELIVERING(2), DELIVERED(3)
+
+    private int number;
+
+    OrderStatus(int number){
+        this.number = number;
+    }
 
 }
