ec0b62174d12fc0619aa7a0170034cbe00ce54df
SeonHakJi <jshag90@naver.com>
2022-07-18 20:54:24 +0900

    [Add] 완벽공략 10. 가변 인수

---
+++ src/com/ji/effective/java/item02/VarargsSamples.java
@@ -0,0 +1,21 @@
+package com.ji.effective.java.item02;
+
+import java.util.Arrays;
+
+/**
+ * 완벽 공략 10 - 가변 인수
+ */
+public class VarargsSamples {
+
+    public void printNumbers(int... numbers){
+        System.out.println(numbers.getClass().getCanonicalName());
+        System.out.println(numbers.getClass().getComponentType());
+        Arrays.stream(numbers).forEach(System.out::println);
+    }
+
+    //Heap pollution
+    public static void main(String[] args) {
+        VarargsSamples samples = new VarargsSamples();
+        samples.printNumbers(5, 10);
+    }
+}
