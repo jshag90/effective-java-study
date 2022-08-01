f66b5460f59730d127bffc67dd9af2c97595eefe
SeonHakJi <jshag90@naver.com>
2022-08-01 17:58:18 +0900

    [Add] 아이템 7. 완벽 공략 19 - NullPointerException

---
+++ src/main/java/com/ji/effective/java/item07/optional/Channel.java
@@ -0,0 +1,20 @@
+package com.ji.effective.java.item07.optional;
+
+import me.whiteship.chapter01.item07.optional.MemberShip;
+
+import java.util.Optional;
+
+public class Channel {
+
+    private int numOfSubscribers;
+
+    public Optional<MemberShip> defaultMemberShip(){
+        if(this.numOfSubscribers < 2000){
+            return Optional.empty();
+        }else{
+            return Optional.of(new MemberShip());
+        }
+
+    }
+
+}
---
+++ src/main/java/com/ji/effective/java/item07/optional/MemberShip.java
@@ -0,0 +1,7 @@
+package com.ji.effective.java.item07.optional;
+
+public class MemberShip {
+
+    public String hello(){ return "hello";}
+
+}
