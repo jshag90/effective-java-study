034d07556740284150d22f47c9a588141e6b1a54
SeonHakJi <jshag90@naver.com>
2022-08-03 23:59:03 +0900

    [Add] 아이템 7. 완벽 공략 21 - Executor 예제 1

---
+++ src/main/java/com/ji/effective/java/item07/executor/ExecutorsExample.java
@@ -0,0 +1,33 @@
+package com.ji.effective.java.item07.executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorsExample {
+    public static void main(String[] args) {
+        int numberOfCpu = Runtime.getRuntime().availableProcessors();
+        System.out.println(numberOfCpu);
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfCpu);
+        for(int i = 0; i<100; i++){
+            executorService.submit(new Task());
+        }
+
+        System.out.println(Thread.currentThread()+ "hello");
+        executorService.shutdown();
+
+    }
+
+    static class Task implements Runnable{
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(2000);
+                System.out.println(Thread.currentThread()+"world");
+            }catch(Exception exception){
+                exception.printStackTrace();
+            }
+        }
+    }
+
+}
