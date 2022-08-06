eb9b5cb0098ce3bff4b2d2e52730c42d59ccb6b8
SeonHakJi <jshag90@naver.com>
2022-08-06 14:50:42 +0900

    [Add] 아이템 7. 완벽공략 WeakHashMap 예제

---
+++ src/main/java/com/ji/effective/java/item07/cache/PostRepository.java
@@ -0,0 +1,28 @@
+package com.ji.effective.java.item07.cache;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
+public class PostRepository {
+
+    private Map<CacheKey, Post> cache;
+
+    public PostRepository() {
+        this.cache = new WeakHashMap<>();
+    }
+
+    public Post getPostById(CacheKey key) {
+        if (cache.containsKey(key)) {
+            return cache.get(key);
+        } else {
+            Post post = new Post();
+            cache.put(key, post);
+            return post;
+        }
+    }
+
+    public Map<CacheKey, Post> getCache() {
+        return cache;
+    }
+
+}
